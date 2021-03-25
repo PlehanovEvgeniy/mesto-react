b4bb7cd00e69b63d39c9928826778c09b2e426d3
PlehanovEvgeniy <72038550+PlehanovEvgeniy@users.noreply.github.com>
2021-03-25 22:13:08 +0300

    fix #4

--- src/components/App.js
+++ src/components/App.js
@@ -36,79 +36,81 @@ const App = () => {
         setSelectedCard(null);
     }
 
-        return (
-        <div className="page">
-            <Header/>
-            <Main
-                onAddPlace={handleAddPlaceClick}
-                onEditAvatar={handleEditAvatarClick}
-                onEditProfile={handleEditProfileClick}
-                onCardClick={handleCardClick}
-            />
-            <Footer/>
+    return (
+        <div className="app">
+            <div className="page">
+                <Header/>
+                <Main
+                    onAddPlace={handleAddPlaceClick}
+                    onEditAvatar={handleEditAvatarClick}
+                    onEditProfile={handleEditProfileClick}
+                    onCardClick={handleCardClick}
+                />
+                <Footer/>
 
-            <PopupWithForm
-                title={"Вы уверены?"}
-                name={"delete-card"}
-                textBtn={"Да"}
-                isOpen={false}
-                onClose={closeAllPopups}
-            >
-            </PopupWithForm>
+                <PopupWithForm
+                    title={"Вы уверены?"}
+                    name={"delete-card"}
+                    textBtn={"Да"}
+                    isOpen={false}
+                    onClose={closeAllPopups}
+                >
+                </PopupWithForm>
 
-            <PopupWithForm
-                title={"Обновить аватар"}
-                name={"avatar-image"}
-                textBtn={"Сохранить"}
-                isOpen={isEditAvatarPopupOpen}
-                onClose={closeAllPopups}
-            >
-                <fieldset className="form__content form__avatar-info">
-                    <input name="avatar" id="user-avatar" className="form__item form__item_type_card-name" placeholder="Ссылка на картинку" type="url" noValidate required />
-                    <span id="user-avatar-error" className="form__item-error"/>
-                </fieldset>
-            </PopupWithForm>
+                <PopupWithForm
+                    title={"Обновить аватар"}
+                    name={"avatar-image"}
+                    textBtn={"Сохранить"}
+                    isOpen={isEditAvatarPopupOpen}
+                    onClose={closeAllPopups}
+                >
+                    <fieldset className="form__content form__avatar-info">
+                        <input name="avatar" id="user-avatar" className="form__item form__item_type_card-name" placeholder="Ссылка на картинку" type="url" noValidate required />
+                        <span id="user-avatar-error" className="form__item-error"/>
+                    </fieldset>
+                </PopupWithForm>
 
-            <PopupWithForm
-                title={"Редактировать профиль"}
-                name={"type_edit"}
-                textBtn={"Сохранить"}
-                isOpen={isEditProfilePopupOpen}
-                onClose={closeAllPopups}
-            >
-                <fieldset className="form__content form__user-info">
-                    <input className="form__item form__item_type_name" type="text" name="name" placeholder="Имя"
-                           minLength="2" maxLength="40" id="user-name" noValidate required/>
-                    <span id="user-name-error" className="form__item-error"/>
-                    <input className="form__item form__item_type_job" type="text" name="prof"
-                           placeholder="Вид деятельности" minLength="2" maxLength="200" id="user-job" noValidate
-                           required/>
-                    <span id="user-job-error" className="form__item-error"/>
-                </fieldset>
-            </PopupWithForm>
+                <PopupWithForm
+                    title={"Редактировать профиль"}
+                    name={"type_edit"}
+                    textBtn={"Сохранить"}
+                    isOpen={isEditProfilePopupOpen}
+                    onClose={closeAllPopups}
+                >
+                    <fieldset className="form__content form__user-info">
+                        <input className="form__item form__item_type_name" type="text" name="name" placeholder="Имя"
+                               minLength="2" maxLength="40" id="user-name" noValidate required/>
+                        <span id="user-name-error" className="form__item-error"/>
+                        <input className="form__item form__item_type_job" type="text" name="prof"
+                               placeholder="Вид деятельности" minLength="2" maxLength="200" id="user-job" noValidate
+                               required/>
+                        <span id="user-job-error" className="form__item-error"/>
+                    </fieldset>
+                </PopupWithForm>
 
-            <PopupWithForm
-                title={"Новое место"}
-                name={"type_add"}
-                textBtn={"Создать"}
-                isOpen={isAddPlacePopupOpen}
-                onClose={closeAllPopups}
-            >
-                <fieldset className="form__content form__add-card">
-                    <input className="form__item form__item_type_card-name" type="text" name="name"
-                           placeholder="Название" minLength="2" maxLength="30" id="card-name" noValidate required/>
-                    <span id="card-name-error" className="form__item-error"/>
-                    <input className="form__item form__item_type_card-link" type="url" name="link"
-                           placeholder="Ссылка на картинку" id="card-link" noValidate required/>
-                    <span id="card-link-error" className="form__item-error"/>
-                </fieldset>
-            </PopupWithForm>
+                <PopupWithForm
+                    title={"Новое место"}
+                    name={"type_add"}
+                    textBtn={"Создать"}
+                    isOpen={isAddPlacePopupOpen}
+                    onClose={closeAllPopups}
+                >
+                    <fieldset className="form__content form__add-card">
+                        <input className="form__item form__item_type_card-name" type="text" name="name"
+                               placeholder="Название" minLength="2" maxLength="30" id="card-name" noValidate required/>
+                        <span id="card-name-error" className="form__item-error"/>
+                        <input className="form__item form__item_type_card-link" type="url" name="link"
+                               placeholder="Ссылка на картинку" id="card-link" noValidate required/>
+                        <span id="card-link-error" className="form__item-error"/>
+                    </fieldset>
+                </PopupWithForm>
 
-            <ImagePopup
-                card={selectedCard}
-                isOpen={selectedCard !== null}
-                onClose={closeAllPopups}
-            />
+                <ImagePopup
+                    card={selectedCard}
+                    isOpen={selectedCard !== null}
+                    onClose={closeAllPopups}
+                />
+            </div>
         </div>
     );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import logo_mesto_img from "../images/logo_mesto.svg"
 const Header = () => {
     return (
         <header className="header">
-            <a className="logo" href="#" target="_blank">
+            <a className="logo" href="/" target="_blank">
                 <img className="logo__picture" src={logo_mesto_img} alt="Логотип Место" />
             </a>
         </header>
